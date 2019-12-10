e437b1fc72cf47beeb820a087b8cd8ef8fcaef8c
Michael-Xie <xiemicha@gmail.com>
2019-12-10 21:19:24 +0000

    Added login routing and stored username to cookie

--- express_server.js
+++ express_server.js
@@ -10,6 +10,7 @@ function generateRandomString() {
 
 }
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 8080; // default port 8080
 
@@ -17,6 +18,7 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -25,6 +27,12 @@ const urlDatabase = {
 
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  console.log(req.body.username);
+  res.redirect("/urls");
 });
 
 app.get("/urls", (req, res) => {
